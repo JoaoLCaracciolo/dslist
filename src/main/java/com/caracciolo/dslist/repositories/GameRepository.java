6dfeca87a1e6eba00ca06c4cc3b163c68e6a24de
João Lucas Caracciolo <caracciolojl@hotmail.com>
2023-05-09 17:07:09 -0300

    Game repository, service, controller

---
+++ src/main/java/com/caracciolo/dslist/repositories/GameRepository.java
@@ -0,0 +1,10 @@
+package com.caracciolo.dslist.repositories;
+
+import com.caracciolo.dslist.entities.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+
+
+
+}
