441111d63b0c8615e1c5226818560333be4d830b
João Lucas Caracciolo <caracciolojl@hotmail.com>
2023-05-09 21:43:32 -0300

    GET game by id

--- src/main/java/com/caracciolo/dslist/services/GameService.java
+++ src/main/java/com/caracciolo/dslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.caracciolo.dslist.services;
 
+import com.caracciolo.dslist.dto.GameDTO;
 import com.caracciolo.dslist.dto.GameMinDTO;
 import com.caracciolo.dslist.entities.Game;
 import com.caracciolo.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +15,13 @@ public class GameService {
 
     @Autowired
     private GameRepository gameRepository;
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        GameDTO dto = new GameDTO(result);
+        return dto;
+    }
 
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
